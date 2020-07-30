b68ad2d9af3e7128ba7c66c1d4c24a437e5db3fc
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2020-07-29 21:00:02 -0700

    swm: 0.2.2 -> 0.3.0

--- pkgs/swm/default.nix
+++ pkgs/swm/default.nix
@@ -2,22 +2,21 @@
 
 buildGoModule rec {
   pname = "swm";
-  version = "0.2.2";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "kalbasit";
     repo = "swm";
     rev = "v${version}";
-    sha256 = "1x5i53y25mmnl8g3zq4bygv9lfcpc5fp49k0na1l35damvs2wi75";
+    sha256 = "1f72sgh4l76s5n9ah26b5716j2qjprii5g7svhs0gmf106277air";
   };
 
-  vendorSha256 = "17nl0akfsz29a4dcwxnrcf4bzqkn4kbwxyxr04j8a4l4fbixr5zr";
+  modSha256 = "1hx16v44bvjxrb6m0m065ac9igv8vh8rj1mhzsiyxnnipkcgm0p0";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/kalbasit/swm";
     description = "swm (Story-based Workflow Manager) is a Tmux session manager specifically designed for Story-based development workflow";
     license = licenses.mit;
     maintainers = [ maintainers.kalbasit ];
-    platforms = platforms.all;
   };
 }
