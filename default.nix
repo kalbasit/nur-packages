d9ff1a3015cf13366ee66abcfb85f85ef252bc2b
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2021-12-27 12:35:45 -0800

    remove dead example left by an upstream merge

--- default.nix
+++ default.nix
@@ -16,7 +16,6 @@ in
 {
   inherit lib modules overlays;
 
-  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 } // (lib.recCallPackage ./pkgs)
