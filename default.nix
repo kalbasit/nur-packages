ba67284128c42ae6b371de7d8d6656ce3a073c74
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2024-06-25 09:47:47 -0700

    import git@github.com:nix-community/nur-packages-template.git as is

--- default.nix
+++ default.nix
@@ -8,14 +8,13 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
-let
+{
+  # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
-in
-{
-  inherit lib modules overlays;
 
+  example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
-} // (lib.recCallPackage ./pkgs)
+}
